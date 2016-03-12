1ec80029d2ff99984d65b2b8faa8249804cf0f11
KarlEbbe <karlebbej@gmail.com>
2016-03-12 14:16:02 +0100

    Uppdateringar

--- src/message/Message.java
+++ src/message/Message.java
@@ -1,70 +1,30 @@
-package p3;
+package message;
 
 import java.io.Serializable;
 
-import javax.swing.Icon;
-
-// Skall den kompletteras med hasPicture()?
-/**
- * Class for messages to be sent between client-server-client.
- * @author KEJ
- *
- */
-public class Message implements Serializable{
-	private String from;
-	private String[] to;
-	private String message;
+public class Message implements Serializable {
+	private String sender;
+	private String[] recipients;
 	private String deliveredToServerTime;
 	private String deliveredFromServerTime;
-	private Icon picture;
 	
 	/**
-	 * Constructor to be used by sending client.
-	 * @param from. The user at the sending client
-	 * @param to. An array of recieving users
-	 * @param message. The text message to send
-	 * @param picture. The picture (Icon) to send.
+	 * Constructor
+	 * @param sender, the name of the sending client
+	 * @param recipients, the recipients. Can be one or many
+	 * @param message, the message to be sent.
 	 */
-	public Message(String from, String[] to, String message, Icon picture){
-		this.from = from;
-		this.to = to;
-		this.message = message;
-		this.picture = picture;
-	}
-	
-	/**
-	 * Constructor to be used by server.
-	 * @param from. The user at the sending client
-	 * @param to. The receiving client
-	 * @param message. The text message to send.
-	 * @param picture. The picture (Icon) to send.
-	 * @param deliveredToServerTime. The time when the message was delivered to/recieved by the server.
-	 * @param deliveredFromServerTime. The time when the message was delivered from the server.
-	 */
-	public Message(String from, String to, String message, Icon picture, String deliveredToServerTime,
-			String deliveredFromServerTime){
-		this.from = from;
-		this.to[0] = to;
-		this.message = message;
-		this.picture = picture;
-		this.deliveredToServerTime = deliveredToServerTime;
-		this.deliveredFromServerTime = deliveredFromServerTime;
-	}
-	
-	/**
-	 * Returns the sending client.
-	 * @return
-	 */
-	public String getFrom(){
-		return from;
+	public Message(String sender, String[] recipients){
+		this.sender = sender;
+		this.recipients = recipients;
 	}
 	
 	/**
 	 * Return an array of recievers of the message.
 	 * @return
 	 */
-	public String[] getTo(){
-		return to;
+	public String[] getRecipients(){
+		return recipients;
 	}
 	
 	/**
@@ -75,27 +35,25 @@ public class Message implements Serializable{
 	}
 	
 	/**
-	 * @return the time when the message was delivered from the server to the recieving client.
+	 * Sets the time when the message was delivered to server.
+	 * @param deliveredToServerTime
+	 */
+	
+	public void setDeliveredToServerTime(String deliveredToServerTime){
+		this.deliveredToServerTime = deliveredToServerTime;
+	}
+	
+	/**
+	 * @return the time when the message was delivered to the server, from the client
 	 */
 	public String getDeliveredFromServerTime(){
 		return deliveredFromServerTime;
 	}
 	
 	/**
-	 * @return the picture (Icon)
+	 * Sets the time when the message is sent from the server.
+	 * @param deliveredFromServerTime
 	 */
-	public Icon getPicture(){
-		return picture;
-	}
-	
-	/**
-	 * Sets the time when the message was delivered to server.
-	 * @param deliveredToServerTime
-	 */
-	public void setDeliveredToServerTime(String deliveredToServerTime){
-		this.deliveredToServerTime = deliveredToServerTime;
-	}
-	
 	public void setSentFromServer(String deliveredFromServerTime){
 		this.deliveredFromServerTime = deliveredFromServerTime;
 	}
