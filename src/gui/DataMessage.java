53c4ac678cd090aa5795aef0232bf6c4405aff0d
KarlEbbe <karlebbej@gmail.com>
2016-03-12 13:33:46 +0100

    Nya message-klasser
    Command message. To be used for e.g. asking server about clients online.
    DataMessage. To be used for e.g. server sending which clients are
    online.

---
+++ src/gui/DataMessage.java
@@ -0,0 +1,15 @@
+package message;
+
+/**
+ * Class representing data to be transfered, e.g. data from server to client. 
+ * @author KEJ
+ *
+ */
+public class DataMessage extends Message{
+	private String[] data;
+	
+	public DataMessage(String sender, String[] recipients, String[] data){
+		super(sender, recipients);
+		this.data = data;
+	}
+}
