09afeb8650666a2995f46540884dc15d1051bdc9
K4LK <simon.d.mansson@gmail.com>
2016-03-12 14:46:45 +0100

    controller updated sendMessage methods

--- src/client/ClientController.java
+++ src/client/ClientController.java
@@ -1,8 +1,7 @@
 package client;
 import gui.ClientGUI;
 import gui.ConnectGUI;
-import message.ChatMessage;
-import message.Message;
+import message.*;
 
 import javax.swing.Icon;
 import javax.swing.JFrame;
@@ -18,62 +17,63 @@ public class ClientController  {
 	private JFrame clientFrame;
 	private ConnectionListener listener;
 	private ClientConnection clientConnection;
-	
+
+
 	public ClientController () {
 		connectGui = new ConnectGUI(this);
 		clientGui = new ClientGUI(this);
 		listener = new ConnectionListener();
 		createConnectFrame();
 	}
-	
-	
+
+
 	public void connect(String address, int port, String userName) {
 		this.userName = userName;
 		clientConnection = new ClientConnection(address, port, listener);
 		clientConnection.startListener();
 		createClientFrame();
-		
+
 	}
 
 	public void createClientFrame() {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
-                clientFrame = new JFrame("Client");
-                clientFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                clientFrame.add(clientGui);
-                clientFrame.pack();
-                clientFrame.setVisible(true);
-            }
-        });
+				clientFrame = new JFrame("Client");
+				clientFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				clientFrame.add(clientGui);
+				clientFrame.pack();
+				clientFrame.setVisible(true);
+			}
+		});
 	}
-	
+
 	public void createConnectFrame() {
-		 SwingUtilities.invokeLater(new Runnable() {
-	            public void run() {
-	                connectFrame = new JFrame("NetworkChat");
-	                connectFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	                connectFrame.add(connectGui);
-	                connectFrame.pack();
-	                connectFrame.setLocation(400, 300);
-	                connectFrame.setVisible(true);
-	            }
-	        });
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				connectFrame = new JFrame("NetworkChat");
+				connectFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				connectFrame.add(connectGui);
+				connectFrame.pack();
+				connectFrame.setLocation(400, 300);
+				connectFrame.setVisible(true);
+			}
+		});
 	}
-	
+
 	public void sendMessage(String chatMessage, String[] recipients) {
-	 new ChatMessage(userName, recipients, chatMessage, null);
-	 
+		clientConnection.sendMessage( new ChatMessage(userName, recipients, chatMessage, null));
+
 	}
-	
+
 	public void sendMessage(String chatMessage, String[] recipients , Icon icon) {
-		new ChatMessage(userName, recipients, chatMessage, icon);
+		clientConnection.sendMessage(new ChatMessage(userName, recipients, chatMessage, icon));
 	}
-	
+
 	public static void main(String[] args) {
 		new ClientController();	
 
 	}
-	
+
 	private class ConnectionListener implements MessageCallback {
 
 		public void add(Message message) {
@@ -87,5 +87,5 @@ public class ClientController  {
 		}
 	}
 
-	
+
 }
