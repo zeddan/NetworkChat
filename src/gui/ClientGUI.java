238c31166f9e4d1c04ef84b0ae2bc3ceab0c4d98
Robin Sääf <robin.saaf@chas.se>
2016-03-14 22:38:43 +0100

    monster refactor of ClientGUI, beware

--- src/gui/ClientGUI.java
+++ src/gui/ClientGUI.java
@@ -1,7 +1,7 @@
 package gui;
 
-import client.ClientController;
 import client.MessageListener;
+import gui.button.CustomButton;
 import message.*;
 import server.User;
 
@@ -9,10 +9,7 @@ import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Map;
@@ -23,178 +20,286 @@ import java.util.Map;
  */
 public class ClientGUI extends JPanel {
 
-	private JPanel pnlLeft = new JPanel(new FlowLayout());
-	private JPanel pnlRight = new JPanel();
-	private JPanel pnlLeftGroups = new JPanel();
-	private JPanel pnlLeftPrivate = new JPanel();
-	private JPanel pnlMain = new JPanel(new BorderLayout());
-	private JTextField tfChatWrite = new JTextField();
-	private JTextArea taChatWindow = new JTextArea();
-	private JTextArea taUserList = new JTextArea();
-	private JLabel lblGroupList = new JLabel("Groups", SwingConstants.CENTER);
-	private JLabel lblGroupCreate = new JLabel("+ Create group", SwingConstants.CENTER);
-	private JLabel lblPrivateList = new JLabel("Private messages", SwingConstants.CENTER);
-	private JLabel lblPrivateCreate = new JLabel("+ New message", SwingConstants.CENTER);
-	private JLabel lblChatUsers = new JLabel("Users", SwingConstants.CENTER);
-	private JScrollPane chatWindowScrollbar = new JScrollPane (taChatWindow);
-	private JScrollPane userListScrollbar = new JScrollPane (taUserList);
+    private static final Integer WIN_WIDTH = 1024;
+    private static final Integer WIN_HEIGHT = 600;
+    private static final Integer PNL_LEFT_WIDTH = WIN_WIDTH/5;
+    private static final Integer PNL_LEFT_HEIGHT = WIN_HEIGHT;
+    private static final Integer PNL_RIGHT_WIDTH = WIN_WIDTH/5;
+    private static final Integer PNL_RIGHT_HEIGHT = WIN_HEIGHT;
 
-	private static final Integer WIN_WIDTH = 1024;
-	private static final Integer WIN_HEIGHT = 600;
-	private static final Integer PNL_LEFT_WIDTH = WIN_WIDTH/5;
-	private static final Integer PNL_LEFT_HEIGHT = WIN_HEIGHT;
-	private static final Integer PNL_RIGHT_WIDTH = WIN_WIDTH/5;
-	private static final Integer PNL_RIGHT_HEIGHT = WIN_HEIGHT;
+    private Dimension pnlLeftSize = new Dimension(PNL_LEFT_WIDTH, PNL_LEFT_HEIGHT);
+    private Dimension pnlLeftGroupsSize = new Dimension(PNL_LEFT_WIDTH, PNL_LEFT_HEIGHT/2);
+    private Dimension pnlLeftPrivateSize = new Dimension(PNL_LEFT_WIDTH, PNL_LEFT_HEIGHT/2);
+    private Dimension pnlRightSize = new Dimension(PNL_RIGHT_WIDTH, PNL_RIGHT_HEIGHT);
 
-	private Dimension pnlLeftSize = new Dimension(PNL_LEFT_WIDTH, PNL_LEFT_HEIGHT);
-	private Dimension pnlLeftGroupsSize = new Dimension(PNL_LEFT_WIDTH, PNL_LEFT_HEIGHT/2);
-	private Dimension pnlLeftPrivateSize = new Dimension(PNL_LEFT_WIDTH, PNL_LEFT_HEIGHT/2);
-	private Dimension pnlRightSize = new Dimension(PNL_RIGHT_WIDTH, PNL_RIGHT_HEIGHT);
+	private JTextArea taUserList;
+    private JTextArea taChatWindow;
 
     private MessageListener listener;
+    private String username;
 
 	public ClientGUI(MessageListener listener) {
         this.listener = listener;
 		setPreferredSize(new Dimension(WIN_WIDTH, WIN_HEIGHT));
 		setLayout(new BorderLayout());
-		setupListeners();
+        setBackground(Color.WHITE);
 
-		// set sizes
-		tfChatWrite.setPreferredSize(new Dimension(WIN_WIDTH/3, WIN_HEIGHT/20));
-		lblGroupList.setPreferredSize(new Dimension(PNL_LEFT_WIDTH, 20));
-		lblPrivateList.setPreferredSize(new Dimension(PNL_LEFT_WIDTH, 20));
-		pnlLeft.setPreferredSize(pnlLeftSize);
-		pnlLeftGroups.setPreferredSize(pnlLeftGroupsSize);
-		pnlLeftPrivate.setPreferredSize(pnlLeftPrivateSize);
-		pnlRight.setPreferredSize(pnlRightSize);
-		chatWindowScrollbar.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-		userListScrollbar.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        // MAIN PANEL
+        JPanel pnlMain = pnlMain();
+        CustomButton btnImageChooser = btnImageChooser();
+        JTextField tfChatWrite = tfChatWrite();
+        taChatWindow = taChatWindow();
+        JScrollPane spChatWindow = spChatWindow(taChatWindow);
+        pnlMain.add(spChatWindow, BorderLayout.CENTER);
+        JPanel pnlChatWrite = pnlChatWrite();
+        pnlChatWrite.add(tfChatWrite, BorderLayout.CENTER);
+        pnlChatWrite.add(btnImageChooser, BorderLayout.EAST);
+        pnlMain.add(pnlChatWrite, BorderLayout.SOUTH);
 
-		// styling and behavior
-		pnlLeft.setBorder(new EmptyBorder(10,30,10,0));
-		pnlRight.setBorder(new EmptyBorder(15,15,10,0));
-		pnlLeftGroups.setLayout(new BoxLayout(pnlLeftGroups, BoxLayout.Y_AXIS));
-		pnlLeftPrivate.setLayout(new BoxLayout(pnlLeftPrivate, BoxLayout.Y_AXIS));
-		pnlRight.setLayout(new BoxLayout(pnlRight, BoxLayout.Y_AXIS));
-		taChatWindow.setBorder(null);
-		tfChatWrite.setBorder(BorderFactory.createCompoundBorder(
-				new LineBorder(new Color(145, 145, 145), 3),
-				new EmptyBorder(5, 5, 5, 5)));
-		pnlMain.setBorder(new EmptyBorder(10, 10, 10, 10));
+        // LEFT PANEL
+        JPanel pnlLeft = pnlLeft();
+        JLabel lblGroupList = lblGroupList();
+        JLabel lblPrivateList = lblPrivateList();
+        JPanel pnlLeftGroups = pnlLeftGroups();
+        JPanel pnlLeftPrivate = pnlLeftPrivate();
+        JLabel lblGroupCreate = lblGroupCreate();
+        JLabel lblPrivateCreate = lblPrivateCreate();
+        pnlLeftGroups.add(lblGroupList);
+        pnlLeftGroups.add(lblGroupCreate);
+        pnlLeftPrivate.add(lblPrivateList);
+        pnlLeftPrivate.add(lblPrivateCreate);
+        pnlLeft.add(pnlLeftGroups);
+        pnlLeft.add(pnlLeftPrivate);
 
-		// create and insert chat window into a JScrollPane
-		JScrollPane scroll = new JScrollPane (taChatWindow);
-	    scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        // RIGHT PANEL
+        JPanel pnlRight = pnlRight();
+        JLabel lblChatUsers = lblChatUsers();
+        taUserList = taUserList();
+        JScrollPane spUserList = spUserList(taUserList);
+        pnlRight.add(lblChatUsers);
+        pnlRight.add(spUserList);
 
-		// add components to left panel
-		pnlLeftGroups.add(lblGroupList);
-		pnlLeftGroups.add(lblGroupCreate);
-		pnlLeftPrivate.add(lblPrivateList);
-		pnlLeftPrivate.add(lblPrivateCreate);
-		pnlLeft.add(pnlLeftGroups);
-		pnlLeft.add(pnlLeftPrivate);
-
-		// add components to right panel
-		pnlRight.add(lblChatUsers);		
-		pnlRight.add(userListScrollbar);
-
-		// panel bg colors
-		pnlLeft.setBackground(new Color(56, 56, 56));
-		pnlRight.setBackground(new Color(56, 56, 56));
-		pnlLeftGroups.setOpaque(false);
-		pnlLeftPrivate.setOpaque(false);
-		pnlMain.setBackground(Color.WHITE);
-		setBackground(Color.WHITE);
-
-		// text colors
-		lblGroupList.setForeground(Color.WHITE);
-		lblGroupCreate.setForeground(new Color(145,145,145));
-		lblPrivateList.setForeground(Color.WHITE);
-		lblPrivateCreate.setForeground(new Color(145,145,145));
-		lblChatUsers.setForeground(Color.WHITE);
-
-		taChatWindow.setEditable(false);
-
-		pnlMain.add(chatWindowScrollbar, BorderLayout.CENTER);
-		pnlMain.add(tfChatWrite, BorderLayout.SOUTH);
-		this.add(pnlLeft, BorderLayout.WEST);
-		this.add(pnlMain, BorderLayout.CENTER);
-		this.add(pnlRight, BorderLayout.EAST);
+		add(pnlLeft, BorderLayout.WEST);
+		add(pnlMain, BorderLayout.CENTER);
+		add(pnlRight, BorderLayout.EAST);
 	}
 
-	public void setupListeners() {
+    private JPanel pnlChatWrite() {
+        JPanel pnlChatWrite = new JPanel(new BorderLayout());
+        return pnlChatWrite;
+    }
 
-		/* * * * * * * * * * *
-            CREATE NEW GROUP
-		 * * * * * * * * * * */
-		lblGroupCreate.addMouseListener(new MouseListener() {
-			@Override
-			public void mouseClicked(MouseEvent mouseEvent) {
-				//  mock start
-				Map<String, User> users = new Hashtable<>();
-				for (int i=0; i < 22; i++) {
-					users.put("user"+i, new UserMock("user"+i));
-				} // mock end
-				ArrayList<User> selectedUsers = NewGroupDialog.display(users);
-				String s = "";
-				if (selectedUsers != null && selectedUsers.size() > 0)
-					for (User u : selectedUsers)
-						s += u.getUserName() + " ";
-				JOptionPane.showMessageDialog(null, s);
-			}
+    private JPanel pnlMain() {
+        JPanel pnlMain = new JPanel(new BorderLayout());
+        pnlMain.setBackground(Color.WHITE);
+        pnlMain.setBorder(new EmptyBorder(10, 10, 10, 10));
+        return pnlMain;
+    }
 
-			@Override
-			public void mousePressed(MouseEvent mouseEvent) {
+    private JScrollPane spChatWindow(JTextArea taChatWindow) {
+        JScrollPane spChatWindow = new JScrollPane (taChatWindow);
+        spChatWindow.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        return spChatWindow;
+    }
 
-			}
+    private JTextArea taChatWindow() {
+        JTextArea taChatWindow = new JTextArea();
+        taChatWindow.setEditable(false);
+        taChatWindow.setBorder(null);
+        return taChatWindow;
+    }
 
-			@Override
-			public void mouseReleased(MouseEvent mouseEvent) {
+    private JTextArea taUserList() {
+        JTextArea taUserList = new JTextArea();
+        return taUserList;
+    }
 
-			}
+    private JScrollPane spUserList(JTextArea taUserList) {
+        JScrollPane spUserList = new JScrollPane(taUserList);
+        spUserList.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        return spUserList;
+    }
 
-			@Override
-			public void mouseEntered(MouseEvent mouseEvent) {
-				lblGroupCreate.setForeground(new Color(250,250,250));
-			}
+    private JLabel lblChatUsers() {
+        JLabel lblChatUsers = new JLabel("Users", SwingConstants.CENTER);
+        lblChatUsers.setForeground(Color.WHITE);
+        return lblChatUsers;
+    }
 
-			@Override
-			public void mouseExited(MouseEvent mouseEvent) {
-				lblGroupCreate.setForeground(new Color(145,145,145));
-			}
-		});
+    private JPanel pnlRight() {
+        JPanel pnlRight = new JPanel();
+        pnlRight.setPreferredSize(pnlRightSize);
+        pnlRight.setBorder(new EmptyBorder(15,15,10,0));
+        pnlRight.setLayout(new BoxLayout(pnlRight, BoxLayout.Y_AXIS));
+        pnlRight.setBackground(new Color(56, 56, 56));
+        return pnlRight;
+    }
 
-		/* * * * * * * * * * *
-            CREATE NEW MESSAGE
-		 * * * * * * * * * * */
-		lblPrivateCreate.addMouseListener(new MouseListener() {
-			@Override
-			public void mouseClicked(MouseEvent mouseEvent) {
-				JOptionPane.showMessageDialog(null, "nothing here yet");
-			}
+    private JLabel lblPrivateCreate() {
+        final JLabel lblPrivateCreate = new JLabel("+ New message", SwingConstants.CENTER);
+        lblPrivateCreate.setForeground(new Color(145,145,145));
+        lblPrivateCreate.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {
+                JOptionPane.showMessageDialog(null, "nothing here yet");
+            }
 
-			@Override
-			public void mousePressed(MouseEvent mouseEvent) {
+            @Override
+            public void mousePressed(MouseEvent mouseEvent) {}
 
-			}
+            @Override
+            public void mouseReleased(MouseEvent mouseEvent) {}
 
-			@Override
-			public void mouseReleased(MouseEvent mouseEvent) {
+            @Override
+            public void mouseEntered(MouseEvent mouseEvent) {
+                lblPrivateCreate.setForeground(new Color(250,250,250));
+            }
 
-			}
+            @Override
+            public void mouseExited(MouseEvent mouseEvent) {
+                lblPrivateCreate.setForeground(new Color(145,145,145));
+            }
 
-			@Override
-			public void mouseEntered(MouseEvent mouseEvent) {
-				lblPrivateCreate.setForeground(new Color(250,250,250));
-			}
+        });
+        return lblPrivateCreate;
+    }
 
-			@Override
-			public void mouseExited(MouseEvent mouseEvent) {
-				lblPrivateCreate.setForeground(new Color(145,145,145));
-			}
+    private JLabel lblGroupCreate() {
+        final JLabel lblGroupCreate = new JLabel("+ Create group", SwingConstants.CENTER);
+        lblGroupCreate.setForeground(new Color(145,145,145));
+        lblGroupCreate.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {
+                //  mock start
+                Map<String, User> users = new Hashtable<>();
+                for (int i=0; i < 22; i++) {
+                    users.put("user"+i, new UserMock("user"+i));
+                } // mock end
+                ArrayList<User> selectedUsers = NewGroupDialog.display(users);
+                String s = "";
+                if (selectedUsers != null && selectedUsers.size() > 0)
+                    for (User u : selectedUsers)
+                        s += u.getUserName() + " ";
+                JOptionPane.showMessageDialog(null, s);
+            }
 
-		});
-	}
+            @Override
+            public void mousePressed(MouseEvent mouseEvent) {}
+
+            @Override
+            public void mouseReleased(MouseEvent mouseEvent) {}
+
+            @Override
+            public void mouseEntered(MouseEvent mouseEvent) {
+                lblGroupCreate.setForeground(new Color(250,250,250));
+            }
+
+            @Override
+            public void mouseExited(MouseEvent mouseEvent) {
+                lblGroupCreate.setForeground(new Color(145,145,145));
+            }
+        });
+        return lblGroupCreate;
+    }
+
+    private JPanel pnlLeftPrivate() {
+        JPanel pnlLeftPrivate = new JPanel();
+        pnlLeftPrivate.setLayout(new BoxLayout(pnlLeftPrivate, BoxLayout.Y_AXIS));
+        pnlLeftPrivate.setPreferredSize(pnlLeftPrivateSize);
+        pnlLeftPrivate.setOpaque(false);
+        return pnlLeftPrivate;
+    }
+
+    private JPanel pnlLeftGroups() {
+        JPanel pnlLeftGroups = new JPanel();
+        pnlLeftGroups.setLayout(new BoxLayout(pnlLeftGroups, BoxLayout.Y_AXIS));
+        pnlLeftGroups.setPreferredSize(pnlLeftGroupsSize);
+        pnlLeftGroups.setOpaque(false);
+        return pnlLeftGroups;
+    }
+
+    private JPanel pnlLeft() {
+        JPanel pnlLeft = new JPanel(new FlowLayout());
+        pnlLeft.setPreferredSize(pnlLeftSize);
+        pnlLeft.setBorder(new EmptyBorder(10,30,10,0));
+        pnlLeft.setBackground(new Color(56, 56, 56));
+        return pnlLeft;
+    }
+
+    private JLabel lblPrivateList() {
+        JLabel lblPrivateList = new JLabel("Private messages", SwingConstants.CENTER);
+        lblPrivateList.setPreferredSize(new Dimension(PNL_LEFT_WIDTH, 20));
+        lblPrivateList.setForeground(Color.WHITE);
+        return lblPrivateList;
+    }
+
+    private JLabel lblGroupList() {
+        JLabel lblGroupList = new JLabel("Groups", SwingConstants.CENTER);
+        lblGroupList.setPreferredSize(new Dimension(PNL_LEFT_WIDTH, 20));
+        lblGroupList.setForeground(Color.WHITE);
+        return lblGroupList;
+    }
+
+    private JTextField tfChatWrite() {
+        JTextField tfChatWrite = new JTextField();
+        tfChatWrite.setPreferredSize(new Dimension(WIN_WIDTH/3-70, WIN_HEIGHT/20));
+        tfChatWrite.setBorder(BorderFactory.createCompoundBorder(
+                new LineBorder(new Color(145, 145, 145), 3),
+                new EmptyBorder(5, 5, 5, 5)));
+        tfChatWrite.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {}
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    //    ChatMessage message = new ChatMessage(
+                    //            username,
+                    //            //recipients,
+                    //            //message,
+                    //            //icon
+                    //            )
+                    //    listener.update(new ChatMessage());
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {}
+        });
+        return tfChatWrite;
+    }
+
+    private CustomButton btnImageChooser() {
+        CustomButton btnImageChooser = new CustomButton("img");
+        btnImageChooser.setPreferredSize(new Dimension(70, WIN_HEIGHT/20));
+        btnImageChooser.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                // open file chooser
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
+        return btnImageChooser;
+    }
 
     public void newChatMessage(ChatMessage message) {
         taChatWindow.setText(message.toString());
@@ -214,20 +319,24 @@ public class ClientGUI extends JPanel {
         }
     }
 
-	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-				JFrame frame = new JFrame("Client");
-				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				frame.add(new ClientGUI(null));
-				frame.pack();
-				frame.setLocation(
-						dim.width/2-frame.getSize().width/2,
-						dim.height/2-frame.getSize().height/2);
-				frame.setVisible(true);
-			}
-		});
-	}
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+                JFrame frame = new JFrame("Client");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.add(new ClientGUI(null));
+                frame.pack();
+                frame.setLocation(
+                        dim.width/2-frame.getSize().width/2,
+                        dim.height/2-frame.getSize().height/2);
+                frame.setVisible(true);
+            }
+        });
+    }
 
 }
