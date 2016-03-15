02950b789c77203bbdf52fb2d37de81d1486bab0
Christoffer Strandberg <Strandberg95@Christoffers-MacBook-Pro.local>
2016-03-15 14:03:37 +0100

    Group contains group name and the recipients

---
+++ src/client/Group.java
@@ -0,0 +1,25 @@
+package message;
+
+public class Group{
+	private String[] recipients;
+	private String groupName;
+	
+	public Group(String[] recipients, String groupName){
+		this.recipients = recipients;
+		this.groupName = groupName;
+	}
+	
+	public String[] getRecipients(){
+		return recipients;
+	}
+	
+	public String getGroupName(){
+		return groupName;
+	}
+	
+	public boolean isEqualTo(String otherGroupName){
+		return groupName.equals(otherGroupName);
+	}
+
+
+}
