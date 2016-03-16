df80c51c94ef8b99c906142a2810fffa46c4817e
Kalk88 <simon.d.mansson@gmail.com>
2016-03-16 19:55:28 +0100

    added UserPanel

---
+++ src/gui/panels/UserPanel.java
@@ -0,0 +1,51 @@
+package gui.panels;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+
+public class UserPanel extends JPanel{
+	private JScrollPane scrollableUserList;
+	private JTextArea taUserList;
+	private JLabel lblChatUsers;
+	
+	public UserPanel (Dimension userPanelSize ) {
+		setPreferredSize(userPanelSize);
+		setBorder(new EmptyBorder(15,15,10,0));
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		setBackground(new Color(56, 56, 56));
+		scrollableUserList = scrollableUserList();
+		lblChatUsers = new JLabel("Users", SwingConstants.CENTER);
+		lblChatUsers.setForeground(Color.WHITE);
+		this.add(lblChatUsers);
+		this.add(scrollableUserList);
+	}
+	
+	public void clearUserPanel() {
+		taUserList.setText("");
+	}
+	public void appendUserPanel(String client) {
+		taUserList.append(client + "\n");
+		
+	}
+
+	private JScrollPane scrollableUserList() {
+		taUserList = new JTextArea();
+		taUserList.setBackground(new Color(56, 56, 56));
+		taUserList.setForeground(new Color(145,145,145));
+		taUserList.setEditable(false);
+		JScrollPane scrollableUserList = new JScrollPane(taUserList);
+		scrollableUserList.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_NEVER);
+		scrollableUserList.setBorder(null);
+		return scrollableUserList;
+	}
+	
+	
+
+}
